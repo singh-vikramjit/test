c38e859831b6d66c400dc5472c047786c6b9f92e
Vikramjit Singh <vikramjitsingh@ucreate.co.in>
2019-03-07 14:00:09 +0530

    create component to use common methoed

---
+++ app/Components/UploadMedia.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Components;
+use Illuminate\Support\Facades\Storage;
+
+class UploadMedia
+{
+    public function __construct(){
+        // 
+    }
+
+    /**
+     * upload image to default Storage disk  
+     */
+    public function uploadImage($image){
+        $filename = rand() . '_' . $image->getClientOriginalName();
+        Storage::putFileAs('', $image, $filename);
+        return $filename;
+    }
+
+    /**
+     * get image URL from default Storage disk  
+     */
+    public function fileUrl($filename){
+        return Storage::url($filename);
+    }
+
+    /**
+     * scale the given image to given scale size   
+     */
+    public function scaleImage($filename, $scale){
+        $scaled_filename = $scale.'_'.$filename;
+        $scaled_file_path = public_path('uploads/'.$scaled_filename);
+        exec('ffmpeg -i '.public_path('uploads/'.$filename).' -vf scale='.$scale.':-1 '.$scaled_file_path);
+        return $scaled_filename;
+    }
+}
--- app/Http/Controllers/UploadMediaController.php
+++ app/Http/Controllers/UploadMediaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use App\Components\UploadMedia;
 
 class UploadMediaController extends Controller
 {
@@ -20,28 +21,14 @@ class UploadMediaController extends Controller
                                     'image' => 'required|image',
                                     'scale' => 'required|numeric|min:9|max:2000',
                                 ]);
-            $filename = $this->uploadImage(request()->file('image'));
-            $orignal_file_url = $this->fileUrl($filename);
-            $scaled_filename = $this->scaleImage($filename, request()->scale);
-            $scaled_file_url = $this->fileUrl($scaled_filename);
+
+            $upload_media = new UploadMedia();
+
+            $filename = $upload_media->uploadImage(request()->file('image'));
+            $orignal_file_url = $upload_media->fileUrl($filename);
+            $scaled_filename = $upload_media->scaleImage($filename, request()->scale);
+            $scaled_file_url = $upload_media->fileUrl($scaled_filename);
         }
         return view('uploadFile', compact(['orignal_file_url','scaled_file_url']));
-    }
-
-    private function uploadImage($image){
-        $filename = rand() . '_' . $image->getClientOriginalName();
-        Storage::putFileAs('', $image, $filename);
-        return $filename;
-    }
-
-    private function fileUrl($filename){
-        return Storage::url($filename);
-    }
-
-    private function scaleImage($filename, $scale){
-        $scaled_filename = $scale.'_'.$filename;
-        $scaled_file_path = public_path('uploads/'.$scaled_filename);
-        exec('ffmpeg -i '.public_path('uploads/'.$filename).' -vf scale='.$scale.':-1 '.$scaled_file_path);
-        return $scaled_filename;
     }
 }
